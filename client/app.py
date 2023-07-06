270f6549b2aae3c4eb3ed205ffb2d38e2510468e
NatVaymer <95870302+NatVaymer@users.noreply.github.com>
2023-07-06 22:03:52 +0300

    Update app.py
    adding checkboxes bringing the popup table to a uniform appearance

--- client/app.py
+++ client/app.py
@@ -7,15 +7,19 @@ from kivy.graphics.texture import Texture
 from pyzbar import pyzbar
 from kivy.clock import Clock
 from kivy.uix.popup import Popup
-from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.spinner import Spinner
 from kivy.uix.gridlayout import GridLayout
 from kivy.metrics import dp
+from kivymd.uix.selectioncontrol import MDCheckbox
 
 import controller
 
+
 class QRCodeScannerApp(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.selected_rows = []  # List to track selected rows
     def build(self):
         layout = BoxLayout(orientation='vertical')
         self.image = Image()
@@ -26,7 +30,7 @@ class QRCodeScannerApp(MDApp):
         self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 1)
         self.texture = None
         self.is_processing_frame = False
-        
+
         search_button = Button(text='Поиск', size_hint=(None, None), size=(100, 50))
         search_button.bind(on_press=self.open_search_window)
         scan_button = Button(text='Сканировать QR', size_hint=(None, None), size=(100, 50))
@@ -44,15 +48,14 @@ class QRCodeScannerApp(MDApp):
 
         self.is_processing_frame = True
         ret, frame = self.capture.read()
-    
+
         if ret:
             # Преобразование кадра в оттенки серого
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             # Распознавание QR-кодов
             decoded_objects = pyzbar.decode(gray)
             print(decoded_objects)
-            
-            
+
             if len(decoded_objects) == 0:  # Если массивы пустые то выполняет отрисовку кадров
                 buf = cv2.flip(frame, 0).tostring()
                 self.texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
@@ -60,53 +63,82 @@ class QRCodeScannerApp(MDApp):
                 self.image.texture = self.texture
             else:
                 last_decoded_object = decoded_objects[0]
-                #иначе не пустой пойманный массив декодирует 
+                # иначе не пустой пойманный массив декодирует
                 text = last_decoded_object.data.decode('utf-8')
-                
+
                 container_id = text[-1]
                 self.show_table_popup(container_id)
                 return
 
         self.is_processing_frame = False
 
-    def show_table_popup(self, text):
 
+    def increase_selected_quantity(self, row_data):
+        for row in row_data:
+            checkbox = row[0]
+            print(checkbox)
+            if checkbox.active:
+                quantity_label = row[-1]  # Assuming "Количество" column is the last item
+                quantity = int(quantity_label.text)
+                quantity += 1  # Increase the quantity by 1
+                quantity_label.text = str(quantity)
+
+    def show_table_popup(self, text):
         container_id = text
         product_data = controller.get_container(container_id)
 
         values = []
 
         for product in product_data:
-            values.append([product['name'], product['type'], product['quantity']])
+            checkbox = MDCheckbox(size_hint=(None, None), size=(dp(48), dp(48)))
+            row_data = [checkbox, product['name'], product['type'], product['quantity']]
+            values.append(row_data)
 
         table_data = values
-        
+
         table_layout = MDDataTable(
-            pos_hint = {'center_x': 0.5, 'center_y': 0.5},
+            pos_hint={'center_x': 0.5, 'center_y': 0.5},
             size_hint=(0.8, 0.8),
             use_pagination=True,
-            rows_num = 3,
+            rows_num=3,
+            check=True,  # Enable row selection with checkboxes
             column_data=[
-                #("Номер", dp(30)),
+                ("", dp(48)),  # Checkbox column
                 ("Название", dp(30)),
                 ("Тип", dp(30)),
-                #("Емкость", dp(30)),
-                #("Напряжение", dp(30)),
-                #("Сопротивление", dp(30)),
                 ("Количество", dp(30)),
             ],
             row_data=table_data,
         )
+
         close_button = Button(text='Закрыть', size_hint=(1, 0.2))
         close_button.bind(on_press=self.close_popup)
 
-        table_layout.add_widget(close_button)
+        increase_button = Button(text='Увеличить', size_hint=(1, 0.2))
+        increase_button.bind(on_press=lambda instance: self.increase_selected_quantity(table_data))
 
-        table_popup = Popup(title=f"Содержимое контейнера № {container_id}", content=table_layout, size_hint=(0.8, 0.8))
+        button_layout = GridLayout(cols=2, size_hint=(1, None), height=dp(48))
+        button_layout.add_widget(close_button)
+        button_layout.add_widget(increase_button)
+
+        layout = BoxLayout(orientation='vertical')
+        layout.add_widget(table_layout)
+        layout.add_widget(button_layout)
+
+        table_popup = Popup(title=f"Содержимое контейнера № {container_id}", content=layout, size_hint=(0.8, 0.8))
         table_popup.bind(on_press=self.close_popup)
 
-        table_popup.open()
+        self.selected_rows = []  # Clear the selected rows list
 
+        def on_row_select(instance, r):
+            if r.active:
+                self.selected_rows.append(r)
+            else:
+                self.selected_rows.remove(r)
+
+        table_layout.bind(on_row_select=on_row_select)  # Bind the event handler after creating the layout
+
+        table_popup.open()
 
     def close_popup(self, instance):
         instance.parent.parent.parent.parent.dismiss()
@@ -116,6 +148,7 @@ class QRCodeScannerApp(MDApp):
         Clock.schedule_once(self.schedule_next_frame, 0)
 
     '''
+
     def open_search_window(self, instance):
         search_layout = BoxLayout(orientation='vertical')
         containers_tuple = controller.get_all_containers()
@@ -134,42 +167,11 @@ class QRCodeScannerApp(MDApp):
 
         search_popup = Popup(title='Search', content=search_layout, size_hint=(0.6, 0.4))
         search_popup.open()
-        
 
     def on_spinner_select(self, instance, text):
-        self.display_table(text)
+        self.show_table_popup(text)
 
-    def display_table(self, text):
-        container_id = text
-        product_data = controller.get_container(container_id)
 
-        values = []
-
-        for product in product_data:
-            values.append([product['name'], product['type'], product['quantity']])
-
-        table_data = values
-        
-        table_layout = MDDataTable(
-            pos_hint = {'center_x': 0.5, 'center_y': 0.5},
-            size_hint=(0.8, 0.8),
-            use_pagination=True,
-            rows_num = 3,
-            column_data=[
-                #("Номер", dp(30)),
-                ("Название", dp(30)),
-                ("Тип", dp(30)),
-                #("Емкость", dp(30)),
-                #("Напряжение", dp(30)),
-                #("Сопротивление", dp(30)),
-                ("Количество", dp(30)),
-            ],
-            row_data=table_data,
-        )
-
-        table_popup = Popup(title='Table', content=table_layout, size_hint=(0.8, 0.8))
-        table_popup.open()
-        
 
     def change_quantity(self, label, change):
         quantity = int(label.text)
