c6464f8074d3579e1ff2610fd9b8ef41f6f74fed
Archee <archee.busy@gmail.com>
2023-07-06 16:11:48 +0300

    requests, tables, qr changes

--- client/app.py
+++ client/app.py
@@ -1,5 +1,6 @@
 import cv2
-from kivy.app import App
+from kivymd.app import MDApp
+from kivymd.uix.datatables import MDDataTable
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.graphics.texture import Texture
@@ -10,8 +11,11 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.spinner import Spinner
 from kivy.uix.gridlayout import GridLayout
+from kivy.metrics import dp
 
-class QRCodeScannerApp(App):
+import controller
+
+class QRCodeScannerApp(MDApp):
     def build(self):
         layout = BoxLayout(orientation='vertical')
         self.image = Image()
@@ -22,57 +26,106 @@ class QRCodeScannerApp(App):
         self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 1)
         self.texture = None
         self.is_processing_frame = False
-        # Возможно стоит изменить например на 1/60... Нужно настраивать под конкретный телефон.
-        Clock.schedule_interval(self.schedule_next_frame, 1.0 / 30)
-
-        search_button = Button(text='Search', size_hint=(None, None), size=(100, 50))
+        
+        search_button = Button(text='Поиск', size_hint=(None, None), size=(100, 50))
         search_button.bind(on_press=self.open_search_window)
+        scan_button = Button(text='Сканировать QR', size_hint=(None, None), size=(100, 50))
+        scan_button.bind(on_press=self.process_frame)
         layout.add_widget(search_button)
+        layout.add_widget(scan_button)
 
         return layout
 
-    def schedule_next_frame(self, dt):
+    def process_frame(self, dt):
+        # Возможно стоит изменить например на 1/60... Нужно настраивать под конкретный телефон.
+
         if not self.is_processing_frame:
             Clock.schedule_once(self.process_frame)
 
-    def process_frame(self, dt):
         self.is_processing_frame = True
         ret, frame = self.capture.read()
+    
         if ret:
             # Преобразование кадра в оттенки серого
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             # Распознавание QR-кодов
             decoded_objects = pyzbar.decode(gray)
-            for obj in decoded_objects:
-                text = obj.data.decode('utf-8')
-                self.show_popup(text)
-            buf = cv2.flip(frame, 0).tostring()
-            self.texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
-            self.texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
-            self.image.texture = self.texture
+            print(decoded_objects)
+            
+            
+            if len(decoded_objects) == 0:  # Если массивы пустые то выполняет отрисовку кадров
+                buf = cv2.flip(frame, 0).tostring()
+                self.texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+                self.texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+                self.image.texture = self.texture
+            else:
+                last_decoded_object = decoded_objects[0]
+                #иначе не пустой пойманный массив декодирует 
+                text = last_decoded_object.data.decode('utf-8')
+                
+                container_id = text[-1]
+                self.show_table_popup(container_id)
+                return
+
         self.is_processing_frame = False
 
-    def show_popup(self, text):
-        content = BoxLayout(orientation='vertical')
-        content.add_widget(Label(text=text))
-        close_button = Button(text='Close', size_hint=(1, 0.2))
+    def show_table_popup(self, text):
+
+        container_id = text
+        product_data = controller.get_container(container_id)
+
+        values = []
+
+        for product in product_data:
+            values.append([product['name'], product['type'], product['quantity']])
+
+        table_data = values
+        
+        table_layout = MDDataTable(
+            pos_hint = {'center_x': 0.5, 'center_y': 0.5},
+            size_hint=(0.8, 0.8),
+            use_pagination=True,
+            rows_num = 3,
+            column_data=[
+                #("Номер", dp(30)),
+                ("Название", dp(30)),
+                ("Тип", dp(30)),
+                #("Емкость", dp(30)),
+                #("Напряжение", dp(30)),
+                #("Сопротивление", dp(30)),
+                ("Количество", dp(30)),
+            ],
+            row_data=table_data,
+        )
+        close_button = Button(text='Закрыть', size_hint=(1, 0.2))
         close_button.bind(on_press=self.close_popup)
-        content.add_widget(close_button)
-        popup = Popup(title='QR Code', content=content, size_hint=(0.8, 0.8))
-        popup.bind(on_dismiss=self.on_popup_dismiss)
-        popup.open()
+
+        table_layout.add_widget(close_button)
+
+        table_popup = Popup(title=f"Содержимое контейнера № {container_id}", content=table_layout, size_hint=(0.8, 0.8))
+        table_popup.bind(on_press=self.close_popup)
+
+        table_popup.open()
+
 
     def close_popup(self, instance):
         instance.parent.parent.parent.parent.dismiss()
 
+    '''
     def on_popup_dismiss(self, instance):
         Clock.schedule_once(self.schedule_next_frame, 0)
 
+    '''
     def open_search_window(self, instance):
         search_layout = BoxLayout(orientation='vertical')
+        containers_tuple = controller.get_all_containers()
+
+        # Извлечение значений id из containers_tuple
+        ids = [item[0] for item in containers_tuple]
+
         spinner = Spinner(
             text='Select an item',
-            values=('конденсатор', 'транзистор', 'диод'),
+            values=ids,
             size_hint=(None, None),
             size=(200, 50)
         )
@@ -81,38 +134,42 @@ class QRCodeScannerApp(App):
 
         search_popup = Popup(title='Search', content=search_layout, size_hint=(0.6, 0.4))
         search_popup.open()
+        
 
     def on_spinner_select(self, instance, text):
-        if text == 'диод':
-            self.display_table()
+        self.display_table(text)
 
-    def display_table(self):
+    def display_table(self, text):
+        container_id = text
+        product_data = controller.get_container(container_id)
 
-        table_layout = GridLayout(cols=5, size_hint=(None, None), size=(600, 400), spacing=5, padding=5)
-        table_layout.bind(minimum_width=table_layout.setter('width'))
-        #table_layout = GridLayout(cols=5, size_hint=(None, None), size=(600, 400), spacing=5, padding=5)
+        values = []
 
-        table_layout.add_widget(Label(text='Название', bold=True, size_hint_x=None, width=100))
-        table_layout.add_widget(Label(text='Характеристика', bold=True, size_hint_x=None, width=150))
-        table_layout.add_widget(Label(text='Сопротивление', bold=True, size_hint_x=None, width=100))
-        table_layout.add_widget(Label(text='Количество', bold=True, size_hint_x=None, width=100))
-        table_layout.add_widget(Label(text=' ', size_hint_x=None, width=50))  # Пустой столбец для кнопок
+        for product in product_data:
+            values.append([product['name'], product['type'], product['quantity']])
 
-        table_layout.add_widget(Label(text='Диод 1'))
-        table_layout.add_widget(Label(text='Характеристика 1'))
-        table_layout.add_widget(Label(text='100 Ом'))
-        quantity_label = Label(text='0')
-        increase_button = Button(text='+', size_hint=(None, None), size=(50, 30))
-        increase_button.bind(on_press=lambda instance: self.change_quantity(quantity_label, 1))
-        decrease_button = Button(text='-', size_hint=(None, None), size=(50, 30))
-        decrease_button.bind(on_press=lambda instance: self.change_quantity(quantity_label, -1))
-        table_layout.add_widget(quantity_label)
-        table_layout.add_widget(increase_button)
-        table_layout.add_widget(decrease_button)
+        table_data = values
+        
+        table_layout = MDDataTable(
+            pos_hint = {'center_x': 0.5, 'center_y': 0.5},
+            size_hint=(0.8, 0.8),
+            use_pagination=True,
+            rows_num = 3,
+            column_data=[
+                #("Номер", dp(30)),
+                ("Название", dp(30)),
+                ("Тип", dp(30)),
+                #("Емкость", dp(30)),
+                #("Напряжение", dp(30)),
+                #("Сопротивление", dp(30)),
+                ("Количество", dp(30)),
+            ],
+            row_data=table_data,
+        )
+
         table_popup = Popup(title='Table', content=table_layout, size_hint=(0.8, 0.8))
         table_popup.open()
-
-
+        
 
     def change_quantity(self, label, change):
         quantity = int(label.text)
--- client/controller.py
+++ client/controller.py
@@ -1,1 +1,18 @@
 import requests
+
+def get_all_containers():
+    url = "http://127.0.0.1:8000/containers/"
+    response = requests.get(url)
+    data = response.json()
+    
+    # Преобразование словаря в кортеж
+    result = tuple(data.items())
+    
+    return result
+
+def get_container(container_id):
+    url = f"http://127.0.0.1:8000/containers/{container_id}"
+    response = requests.get(url)
+    data = response.json()
+    
+    return data
