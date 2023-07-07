babc829a9d7e8be519aa0c6dadec38af975549b5
NatVaymer <95870302+NatVaymer@users.noreply.github.com>
2023-07-07 09:48:41 +0300

    Update controller.py

--- client/controller.py
+++ client/controller.py
@@ -1,18 +1,27 @@
 import requests
 
+
 def get_all_containers():
     url = "http://127.0.0.1:8000/containers/"
     response = requests.get(url)
     data = response.json()
-    
+
     # Преобразование словаря в кортеж
     result = tuple(data.items())
-    
+
     return result
+
 
 def get_container(container_id):
     url = f"http://127.0.0.1:8000/containers/{container_id}"
     response = requests.get(url)
     data = response.json()
-    
+
+    return data
+
+def increase_product_quantity(container_id: int, product_id: int):
+    url = f"http://127.0.0.1:8000//products/{container_id}/{product_id}/increase"
+    response = requests.get(url)
+    data = response.json()
+
     return data
